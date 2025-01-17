1c721b495ef94a31a89dcce47b965f4e1d49c6cf
amanbhoria <51995611+amanbhoria@users.noreply.github.com>
2025-01-17 17:17:21 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html", 
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}", // Make sure to include all file extensions in src
   ],
   theme: {
-    extend: {
-      colors: {
-        monokai: {
-          background: "#272822",
-          foreground: "#F8F8F2",
-          comment: "#75715E",
-          highlight: "#49483E",
-          // Monokai accent colors
-          pink: "#F92672",
-          green: "#A6E22E",
-          orange: "#FD971F",
-          cyan: "#66D9EF",
-          yellow: "#E6DB74",
-          // etc. if you'd like more
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
