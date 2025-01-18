6a58af7e40c16fcc21bfa9a9aabeb643261fa0f8
amanbhoria <51995611+amanbhoria@users.noreply.github.com>
2025-01-19 02:36:30 +0530

    bugs resolved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,45 @@
-const Navbar = () => {
-    const scrollToSection = (id) => {
-        const section = document.getElementById(id);
-        if (section) {
-            section.scrollIntoView({ behavior: "smooth" });
-        }
-    };
+import React from "react";
+import { FaHome, FaTerminal, FaCogs, FaTwitter, FaGithub, FaExchangeAlt } from "react-icons/fa";
+import { FloatingDock } from "./ui/floating-dock.jsx";
+import { FcAbout, FcContacts } from "react-icons/fc";
+import { FaX } from "react-icons/fa6";
+
+export default function Navbar() {
+
+    const links = [
+        {
+            title: "Home",
+            icon: <FaHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+            href: "#",
+        },
+        {
+            title: "Contact",
+            icon: <FcContacts className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+            href: "#contact",
+        },
+        {
+            title: "About",
+            icon: <FcAbout className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+            href: "#about",
+        },
+        {
+            title: "X",
+            icon: <FaX className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+            href: "https://www.x.com/be_creative007",
+        },
+        {
+            title: "GitHub",
+            icon: <FaGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+            href: "https://www.github.com/amanbhoria",
+        },
+    ];
 
     return (
-        <nav className="flex justify-center mt-4 py-4">
-            <div className="flex space-x-4 bg-black rounded-full p-4">
-                <button
-                    onClick={() => scrollToSection("home")}
-                    className="text-white py-2 px-4 rounded hover:text-gray-400 transition-colors"
-                >
-                    Home
-                </button>
-                <button
-                    onClick={() => scrollToSection("about")}
-                    className="text-white py-2 px-4 rounded hover:text-gray-400 transition-colors"
-                >
-                    About
-                </button>
-                <button
-                    onClick={() => scrollToSection("contact")}
-                    className="text-white py-2 px-4 rounded hover:text-gray-400 transition-colors"
-                >
-                    Contact
-                </button>
-            </div>
-        </nav>
+        <div className="flex items-center pt-20 w-full">
+            <FloatingDock
+                mobileClassName="translate-y-20"
+                items={links}
+            />
+        </div>
     );
-};
-
-export default Navbar;
+}
