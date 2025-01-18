de7cdbecd151445f8d63a31db2bab29bda60cb93
amanbhoria <51995611+amanbhoria@users.noreply.github.com>
2025-01-18 23:48:45 +0530

    restore point

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import TagCloud from '../components/TagCloud.jsx';
 const Home = () => {
     return (
         <div>
-            <img class="w-full h-auto pt-24 grayscale brightness-90 contrast-110 hover:grayscale-0 hover:brightness-100 hover:contrast-100 transition-all duration-300" src="/banner.jpg" alt="banner" />
+            <img className="w-full h-auto pt-24 grayscale brightness-90 contrast-110 hover:grayscale-0 hover:brightness-100 hover:contrast-100 transition-all duration-300" src="/banner.jpg" alt="banner" />
             <TagCloud />
             <About />
             <Blogs />
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/dev-portfolio/', // Important: the slash at the beginning AND end
   plugins: [react()],
 })
