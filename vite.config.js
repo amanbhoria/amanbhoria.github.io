a896beddedf51fca63147510d7c88bb2e3c92370
amanbhoria <51995611+amanbhoria@users.noreply.github.com>
2025-01-19 00:18:34 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  build: {
+    // Optional: output to "docs" so you can serve from there on the main branch
+    outDir: 'docs'
+  }
 })
