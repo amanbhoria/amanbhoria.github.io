299cfc0c941415eeb40445f352a49f9bd9e30995
amanbhoria <51995611+amanbhoria@users.noreply.github.com>
2025-01-18 23:21:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/dev-portfolio/', // Important: the slash at the beginning AND end
   plugins: [react()],
 })
