f1dd0ecf84b25fce902afe2d87cefe599c18e511
amanbhoria <51995611+amanbhoria@users.noreply.github.com>
2025-01-20 21:10:32 +0530

    added shimmer on image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Blogs from "../components/Blogs";
 import GitHubRepos from "../components/GithubRepos.jsx";
 import TagCloud from "../components/TagCloud.jsx";
 import Contact from "./Contact.jsx";
+import { Image, Breathing, Shimmer } from "react-shimmer";
 
 const Home = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -45,11 +46,24 @@ const Home = () => {
 
     return (
         <div>
-            <img
-                className="w-full h-auto pt-24 grayscale brightness-90 contrast-110 hover:grayscale-0 hover:brightness-100 hover:contrast-100 transition-all duration-300"
-                src="/banner2.webp"
+            {/* <img
+                className="w-full pt-24 h-auto grayscale brightness-90 contrast-110 hover:grayscale-0 hover:brightness-100 hover:contrast-100 transition-all duration-300"
+                src="./banner2.webp"
                 alt="banner"
-            />
+            /> */}
+            <div className="pt-20"><Image
+                className="w-full pt-24 h-auto grayscale brightness-90 contrast-110 hover:grayscale-0 hover:brightness-100 hover:contrast-100 transition-all duration-300"
+                src="./banner2.webp"
+                fallback={
+                    <Shimmer
+                        className="w-full pt-24 h-auto grayscale brightness-90 contrast-110 hover:grayscale-0 hover:brightness-100 hover:contrast-100 transition-all duration-300"
+                        width="100%"
+                        height={2560}
+                    />
+                }
+                alt="banner"
+            /></div>
+
             <TagCloud />
             <About />
             <Blogs />
